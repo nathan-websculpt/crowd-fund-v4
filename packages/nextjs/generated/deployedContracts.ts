a91d3d9f274c11f59f73488c06871f8c16723396
nathan-websculpt <websculpt.nathan@gmail.com>
2023-11-03 15:11:38 -0500

    3-wallet FE tests work

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -5,7 +5,7 @@ const contracts = {
       name: "localhost",
       contracts: {
         CrowdFund: {
-          address: "0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E",
+          address: "0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690",
           abi: [
             {
               inputs: [
