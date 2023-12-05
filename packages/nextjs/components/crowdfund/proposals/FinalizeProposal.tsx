969cf1632c5cb21a542ee0b13057610191e4bbe1
nathan-websculpt <websculpt.nathan@gmail.com>
2023-12-05 10:24:14 -0600

    multisig tx (finalize proposal) wrks with subgraph

--- packages/nextjs/components/crowdfund/proposals/FinalizeProposal.tsx
+++ packages/nextjs/components/crowdfund/proposals/FinalizeProposal.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
 import { formatEther } from "viem";
 import getNonce from "~~/helpers/getNonce";
 import { useScaffoldContractRead, useScaffoldContractWrite, useScaffoldEventSubscriber } from "~~/hooks/scaffold-eth";
@@ -16,11 +17,45 @@ export const FinalizeProposal = (proposal: FinalizeProposalProps) => {
   const [nonce, setNonce] = useState<bigint>();
   const tx = { amount: proposal.amount, to: proposal.to, proposedBy: proposal.proposedBy, reason: proposal.reason };
 
+  const [signaturesList, setSignaturesList] = useState<string[]>();
+
+  const PROPOSAL_GRAPHQL = gql`
+    query ($slug1: Int!, $slug2: Int!) {
+      proposalCreateds(where: { fundRunId: $slug1, proposalId: $slug2 }) {
+        signature
+      }
+      proposalSupporteds(where: { fundRunId: $slug1, proposalId: $slug2 }) {
+        signature
+      }
+    }
+  `;
+  const [getProposal, { loading, error, data }] = useLazyQuery(PROPOSAL_GRAPHQL);
+
   useEffect(() => {
     if (nonce !== undefined) {
-      writeAsync();
+      console.log("getting sigs");
+      getProposal({ variables: { slug1: proposal.fundRunId, slug2: proposal.proposalId } });
     }
   }, [nonce]);
+
+  useEffect(() => {
+    console.log("pre-check", data);
+    if (data !== undefined) {
+      console.log(data);
+      let thisArr = [];
+      thisArr.push(data.proposalCreateds[0].signature);
+      for (let i = 0; i < data.proposalSupporteds.length; i++) {
+        thisArr.push(data.proposalSupporteds[i].signature);
+      }
+      setSignaturesList(thisArr);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (signaturesList !== undefined && signaturesList.length > 0) {
+      writeAsync();
+    }
+  }, [signaturesList]);
 
   useScaffoldEventSubscriber({
     contractName: "CrowdFund",
@@ -56,7 +91,7 @@ export const FinalizeProposal = (proposal: FinalizeProposalProps) => {
   const { writeAsync, isLoading } = useScaffoldContractWrite({
     contractName: "CrowdFund",
     functionName: "multisigWithdraw",
-    args: [tx, nonce, proposal.fundRunId, proposal.proposalId],
+    args: [tx, nonce, proposal.fundRunId, proposal.proposalId, signaturesList],
     onBlockConfirmation: txnReceipt => {
       console.log("📦 Transaction blockHash", txnReceipt.blockHash);
       setNonce(undefined);
@@ -66,7 +101,7 @@ export const FinalizeProposal = (proposal: FinalizeProposalProps) => {
       setNonce(undefined);
     },
   });
-
+  if (error) return `Error! ${error}`;//TODO: REMOVE
   return (
     <>
       <td className="w-1/12 text-center md:py-4">
