83e6a66d8b68b11704239384ef5b5d79b4259a1e
nathan-websculpt <websculpt.nathan@gmail.com>
2023-12-20 14:37:52 -0600

    small typo

--- packages/nextjs/pages/queries/index.tsx
+++ packages/nextjs/pages/queries/index.tsx
@@ -3,7 +3,7 @@ import { MetaHeader } from "~~/components/MetaHeader";
 import { DonationsTable } from "~~/components/queries/donationsTable/DonationsTable";
 import { DonorWithdrawalsTable } from "~~/components/queries/donorWithdrawalsTable/DonorWithdrawalsTable";
 import { FundRunsTable } from "~~/components/queries/fundRunsTable/FundRunsTable";
-import { OwnerWithdrawalsTable } from "~~/components/queries/ownerWithdrawalsTable/ownerWithdrawalsTable";
+import { OwnerWithdrawalsTable } from "~~/components/queries/ownerWithdrawalsTable/OwnerWithdrawalsTable";
 import { ProposalsSnapshotTable } from "~~/components/queries/proposalsSnapshotTable/ProposalsSnapshotTable";
 import { SignersSnapshotTable } from "~~/components/queries/signersSnapshotTable/SignersSnapshotTable";
 
