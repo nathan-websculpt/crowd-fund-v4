540d4eeca549e699f92a1bca4eda1e2d077efe3c
nathan-websculpt <websculpt.nathan@gmail.com>
2024-01-31 07:58:54 -0600

    about to deploy to sepolia

--- packages/hardhat/deploy/00_deploy_crowd_fund.ts
+++ packages/hardhat/deploy/00_deploy_crowd_fund.ts
@@ -5,8 +5,7 @@ import { scaffoldDeployWrapper } from "../utils";
 const deployCrowdFund: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
-  // const owner = "0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350"; //todo change to fe wallet
-  const owner = "0x24eA659E7379fe958A36D829a555c3053C393A40"; //todo change to fe wallet
+  const owner = "0x1e7aAbB9D0C701208E875131d0A1cFcDAba79350"; //PRODTODO:
 
   await deploy("CrowdFund", {
     from: deployer,
